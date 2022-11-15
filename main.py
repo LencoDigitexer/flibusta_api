9609f208114ab308755aee6d71bbd3c61ebf7791
LencoDigitexer <addminuser258@gmail.com>
2022-11-15 04:30:29 +0300

    __pycache__ gitignore

--- main.py
+++ main.py
@@ -4,13 +4,49 @@ from bs4 import BeautifulSoup
 from lxml import html
 
 
-domain = "https://flibusta.is/"
+domain = "https://flibusta.is"
+
+
+async def get_book_details(book_id):
+
+    result = requests.get(domain + "/b/" + str(book_id))
+
+    data = BeautifulSoup(result.text, "lxml")
+
+    info = data.select("#main > p")
+
+    for root in info:
+        info = root.get_text()
+
+    image = data.select("#main > img")
+
+    for root in image:
+        image = domain + root["src"]
+
+    download = domain + "/b/" + str(book_id) + "/download"
+    fb2 = domain + "/b/" + str(book_id) + "/fb2"
+    epub = domain + "/b/" + str(book_id) + "/epub"
+    mobi = domain + "/b/" + str(book_id) + "/mobi"
+    pdf = domain + "/b/" + str(book_id) + "/pdf"
+
+    details = {
+        "info": info,
+        "image": image,
+        "download": download,
+        "file" : {
+            "fb2": fb2,
+            "epub": epub,
+            "mobi": mobi,
+            "pdf": pdf,
+        }
+    }
+    return details
 
 
 async def booksearch(request):
     name = request.match_info.get("name", "Anonymous")
 
-    result = requests.get(domain + "booksearch?ask={}&chb=on".format(name))
+    result = requests.get(domain + "/booksearch?ask={}&chb=on".format(name))
 
     data = BeautifulSoup(result.text, "lxml")
 
@@ -29,16 +65,20 @@ async def booksearch(request):
 
         book_a_text = root[0].get_text()
         book_a_href = root[0].get("href")
+        test = await get_book_details(book_a_href[3:])
 
-        res[i]["book"] = {"href": book_a_href, "name": book_a_text}
+        res[i]["book"] = {
+            "name": book_a_text,
+            "details": test,
+        }
 
         for j in range(1, len(root)):
 
             author_a_text = root[j].get_text()
             authors_a_href = root[j].get("href")
 
-            print(author_a_text)
-            print(authors_a_href)
+            # print(author_a_text)
+            # print(authors_a_href)
 
             res[i]["authors"].append({"href": authors_a_href, "name": author_a_text})
         i = i + 1
